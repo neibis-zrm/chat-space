c772d86ad0309c4b5d01c4504fa3902b7f8a22bf
neibis-zrm <berun0d02a46@gmail.com>
2020-07-07 16:40:54 +0900

    Add side-bar latest message

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,17 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :users, through: :user_groups
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.body?
+        last_message.body
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
 end
